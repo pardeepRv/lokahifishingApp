df2cebe74e65644e90d4174dd8e772ba1f14f8a7
dev_rv_219 <iosexperts@rvtechnologies.com>
2021-11-16 17:29:19 +0530

    Tournaments changes code

--- src/screens/NavigationScreens/TournamentHome/TournamentHome.js
+++ src/screens/NavigationScreens/TournamentHome/TournamentHome.js
@@ -1,58 +1,76 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-    FlatList,
-    Image,
-    ImageBackground,
-    SafeAreaView,
-    Text,
-    TouchableOpacity,
-    View
+  FlatList,
+  Image,
+  ImageBackground,
+  Modal,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
-import { moderateScale } from 'react-native-size-matters';
-import { fonts, icons } from '../../../../assets';
-import { Header } from '../../../components/common/Header';
-import { colors } from '../../../utilities/constants';
+import Pdf from 'react-native-pdf';
+import {moderateScale} from 'react-native-size-matters';
+import {fonts, icons} from '../../../../assets';
+import {Header} from '../../../components/common/Header';
+import {colors} from '../../../utilities/constants';
 import styles from './styles';
+
+const source = require('./4th_July_Tournamnt.pdf');
 
 let tournaMents = [
   {
     img: icons.ic_LokahiLogo,
     name: 'Annual Leaderboard',
     date: 'Winners 2020',
+    src: require('../../../../assets/tournament_pdfs/August2020TournamentResults.pdf'),
   },
   {
     img: icons.ic_LokahiLogo,
     name: 'WBC Labour',
     date: 'Day tournament',
+    src: require('../../../../assets/tournament_pdfs/AugustTournamentResult.pdf'),
   },
   {
     img: icons.ic_LokahiLogo,
     name: 'September 2020 Lokahi',
     date: 'App Winners',
+    src: require('../../../../assets/tournament_pdfs/July2020LokahiVirtualAppTournamentWinners.pdf'),
   },
   {
     img: icons.ic_LokahiLogo,
     name: 'dev_pardeep',
     date: 'App Tournament Winners',
+    src: require('../../../../assets/tournament_pdfs/LeaderboardWinners2020.pdf'),
   },
   {
     img: icons.ic_LokahiLogo,
     name: 'September 2020 Lokahi',
     date: 'App Winners',
+    src: require('../../../../assets/tournament_pdfs/LokahiSeptemberWinners.pdf'),
   },
   {
     img: icons.ic_LokahiLogo,
     name: 'dev_pardeep',
     date: 'App Tournament Winners',
+    src: require('../../../../assets/tournament_pdfs/WBC_Labor_Day_Tournament.pdf'),
   },
 ];
 
 const TournamentHome = ({navigation}) => {
   const [tournamentList, settournamentList] = useState(tournaMents);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [pdfPath, setPdfPath] = useState('');
 
   //View of flatlist
   const _renderView = ({item, index}) => (
-    <TouchableOpacity style={styles.listView} activeOpacity={0.8}>
+    <TouchableOpacity
+      onPress={() => {
+        setPdfPath(item.src);
+        setModalVisible(true);
+      }}
+      style={styles.listView}
+      activeOpacity={0.8}>
       <View style={styles.viewStyle}>
         <Image
           source={item.img}
@@ -64,7 +82,7 @@ const TournamentHome = ({navigation}) => {
         <View
           style={{
             justifyContent: 'center',
-            left:10
+            left: 10,
           }}>
           <Text style={styles.nameStyle}>{item.name}</Text>
           <Text style={styles.dateStyle}>{item.date}</Text>
@@ -88,8 +106,7 @@ const TournamentHome = ({navigation}) => {
             height: moderateScale(60),
           }}
           title={'Tournament Result List'}
-          titleStyle={{fontFamily: fonts.bold
-          ,color:colors.black1}}
+          titleStyle={{fontFamily: fonts.bold, color: colors.black1}}
           leftIconSource={icons.ic_back_white}
           leftButtonStyle={{
             tintColor: colors.black1,
@@ -110,6 +127,36 @@ const TournamentHome = ({navigation}) => {
             ) : null
           }
         />
+
+        <Modal
+          animationType="slide"
+          animationType={'slide'}
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {}}>
+          <SafeAreaView style={styles.modal}>
+            <View
+              style={[
+                styles.backBtnView,
+                {flexDirection: 'row', justifyContent: 'space-between'},
+              ]}>
+              <TouchableOpacity
+                style={{width: 100}}
+                onPress={() => {
+                  setModalVisible(false);
+                }}>
+                <Image
+                  source={icons.ic_back_white}
+                  style={{
+                    top: 10,
+                    left: 10,
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+            <Pdf source={pdfPath} style={styles.pdf} loading="Loading PDF..." />
+          </SafeAreaView>
+        </Modal>
       </SafeAreaView>
     </ImageBackground>
   );
--- src/screens/NavigationScreens/TournamentHome/styles.js
+++ src/screens/NavigationScreens/TournamentHome/styles.js
@@ -1,9 +1,11 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet , Dimensions} from 'react-native';
 import {moderateScale} from 'react-native-size-matters';
 import {fonts} from '../../../../assets';
 import {colors} from '../../../utilities/constants';
 import {layout} from '../../../utilities/layout';
 
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
 //external libraries
 import {RFValue} from 'react-native-responsive-fontsize';
 
@@ -41,5 +43,57 @@ export default StyleSheet.create({
     alignSelf: 'center',
     marginTop: 20,
     fontFamily: fonts.semiBold,
+  },
+  modal: {
+		flex: 1,
+		display: 'flex',
+		justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:colors.white1
+  },
+  backBtnView: {
+    width: Dimensions.get('window').width,
+    backgroundColor: colors.primary1,
+    height:moderateScale(50)
+  },
+  modalcontent: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.black4,
+  },
+  backBtn: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: 5,
+		marginLeft: -5,
+		width: '40%',
+	},
+	backBtnText: {
+		fontSize: 17,
+		marginLeft: -10,
+		color: '#147fff',
+  },
+  modalTitle: {
+		textAlign: 'center',
+		fontSize: 20,
+		marginBottom: 15,
+  },
+  pdf: {
+		flex: 1,
+		width: Dimensions.get('window').width * 0.9,
+  },
+
+  modalcontainer: {
+    height: layout.size.height / 5.,
+    width: layout.size.width / 1.5,
+    // top: 60,
+    backgroundColor: colors.lightTransparent,
+    alignItems: 'center',
+    // justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: moderateScale(20),
+    borderColor: '#D3D3D3',
   },
 });

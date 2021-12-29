56e384efc439b5647f7e4e8160a3607cd4ba4104
iosexperts <iosexperts@rvtechnologies.com>
2021-12-29 12:47:37 +0530

    minor changes

--- src/screens/Drawerscreens/FriendProfileScreen/FriendEmergencyContacts.js
+++ src/screens/Drawerscreens/FriendProfileScreen/FriendEmergencyContacts.js
@@ -1,55 +1,48 @@
-
-import React, { useState } from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  Text,
-  ScrollView
-} from 'react-native';
-import { moderateScale } from 'react-native-size-matters';
-import { fonts } from '../../../../assets';
-import { Button } from '../../../components/common';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
+import {fonts} from '../../../../assets';
 import TextInputComp from '../../../components/common/TextInputComp';
 import commonStyles from '../../../utilities/commonStyles';
-
-
-import { colors } from '../../../utilities/constants';
-import { layout } from '../../../utilities/layout';
-
-
-
-
-
+import {colors} from '../../../utilities/constants';
+import {layout} from '../../../utilities/layout';
 
 const FriendEmergencyContacts = Props => {
   console.log(Props, 'props in EmergencyContacts>>>>>>>>>>');
-  const { item, EmergencyContacts } = Props;
+  const {item, EmergencyContacts} = Props;
   console.log(item, 'item in EmergencyContacts>>>>>>>>>>');
   console.log(EmergencyContacts, 'EmergencyContacts lcr  ');
-  const [addContact, setAllContact] = useState([1, 2, 3])
+  const [addContact, setAllContact] = useState([1, 2, 3]);
   const [state, setState] = useState({
-    name:  EmergencyContacts[0].name ? EmergencyContacts[0].name : '',
-    relation: EmergencyContacts[0].relation ? EmergencyContacts[0].relation : '',
-    phoneNo: EmergencyContacts[0].phone_number ? EmergencyContacts[0].phone_number : '',
-    secondName: EmergencyContacts[1].name ? EmergencyContacts[1].name : '',
-    secondRelation:  EmergencyContacts[1].relation ? EmergencyContacts[1].relation : '',
-    secondPhoneNo:EmergencyContacts[1].phone_number ? EmergencyContacts[1].phone_number : ''
+    name:
+      EmergencyContacts && EmergencyContacts.length > 0
+        ? EmergencyContacts[0].name
+        : '',
+    relation:
+      EmergencyContacts && EmergencyContacts.length > 0
+        ? EmergencyContacts[0].relation
+        : '',
+    phoneNo:
+      EmergencyContacts && EmergencyContacts.length > 0
+        ? EmergencyContacts[0].phone_number
+        : '',
+    secondName:
+      EmergencyContacts && EmergencyContacts.length > 1
+        ? EmergencyContacts[1].name
+        : '',
+    secondRelation:
+      EmergencyContacts && EmergencyContacts.length > 1
+        ? EmergencyContacts[1].relation
+        : '',
+    secondPhoneNo:
+      EmergencyContacts && EmergencyContacts.length > 1
+        ? EmergencyContacts[1].phone_number
+        : '',
   });
-  const { name,
-    relation,
-    phoneNo,
-    secondName,
-    secondRelation,
-    secondPhoneNo
-  } = state;
+  const {name, relation, phoneNo, secondName, secondRelation, secondPhoneNo} =
+    state;
 
-  const _onChangeText = key => val => {
-    setState({ ...state, [key]: val });
-  };
-
-  const _renderView = ({ item, index }) => (
+  const _renderView = ({item, index}) => (
     <View
       style={{
         height: layout.size.height / 4,
@@ -57,16 +50,14 @@ const FriendEmergencyContacts = Props => {
         backgroundColor: colors.white1,
         alignSelf: 'center',
         margin: 5,
-        ...commonStyles.shadow
-      }}
-    >
+        ...commonStyles.shadow,
+      }}>
       <Text
         style={{
           fontFamily: fonts.bold,
           color: colors.primary,
-          padding: 5
-        }}
-      >
+          padding: 5,
+        }}>
         Contact no. {index + 1}
       </Text>
 
@@ -78,8 +69,7 @@ const FriendEmergencyContacts = Props => {
           fontFamily: fonts.semiBold,
           fontSize: moderateScale(16),
           color: colors.white1,
-        }}
-        onChangeText={_onChangeText('name')}
+        }} 
       />
     </View>
   );
@@ -87,20 +77,17 @@ const FriendEmergencyContacts = Props => {
   return (
     <ScrollView
       style={{
-        flex: 1
+        flex: 1,
       }}
       contentContainerStyle={{
         paddingBottom: moderateScale(40),
-
       }}
       keyboardShouldPersistTaps={'always'}
       showsVerticalScrollIndicator={false}>
       <View
         style={{
-          flex: 1
-        }}
-      >
-
+          flex: 1,
+        }}>
         <View
           style={{
             height: layout.size.height / 3 + 20,
@@ -108,16 +95,14 @@ const FriendEmergencyContacts = Props => {
             backgroundColor: colors.white1,
             alignSelf: 'center',
             margin: 5,
-            ...commonStyles.shadow
-          }}
-        >
+            ...commonStyles.shadow,
+          }}>
           <Text
             style={{
               fontFamily: fonts.bold,
               color: colors.primary,
-              padding: 5
-            }}
-          >
+              padding: 5,
+            }}>
             Contact no. 1
           </Text>
 
@@ -125,38 +110,32 @@ const FriendEmergencyContacts = Props => {
             editable={false}
             label={'Name'}
             value={name}
-            placeholder={'Please enter name here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
             }}
-            onChangeText={_onChangeText('name')}
           />
 
           <TextInputComp
             editable={false}
             label={'Relation'}
             value={relation}
-            placeholder={'Please enter Relation here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
             }}
-            onChangeText={_onChangeText('relation')}
           />
           <TextInputComp
             editable={false}
             label={'Phone no.'}
             value={phoneNo}
-            placeholder={'Please Phone no. here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
             }}
-            onChangeText={_onChangeText('phoneNo')}
           />
         </View>
         <View
@@ -166,59 +145,50 @@ const FriendEmergencyContacts = Props => {
             backgroundColor: colors.white1,
             alignSelf: 'center',
             margin: 5,
-            ...commonStyles.shadow
-          }}
-        >
+            ...commonStyles.shadow,
+          }}>
           <Text
             style={{
               fontFamily: fonts.bold,
               color: colors.primary,
-              padding: 5
-            }}
-          >
+              padding: 5,
+            }}>
             Contact no. 2
           </Text>
 
           <TextInputComp
-          editable={false}
+            editable={false}
             label={'Name'}
             value={secondName}
-            placeholder={'Please enter name here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
             }}
-            onChangeText={_onChangeText('secondName')}
           />
 
           <TextInputComp
             editable={false}
             label={'Relation'}
             value={secondRelation}
-            placeholder={'Please relation here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
             }}
-            onChangeText={_onChangeText('secondRelation')}
           />
 
           <TextInputComp
             editable={false}
             label={'Phone no.'}
             value={secondPhoneNo}
-            placeholder={'Please phone no. here.'}
             labelTextStyle={{
               fontFamily: fonts.semiBold,
               fontSize: moderateScale(16),
               color: colors.black1,
-            }}
-            onChangeText={_onChangeText('secondPhoneNo')}
+            }} 
           />
         </View>
-
       </View>
     </ScrollView>
   );
@@ -229,11 +199,12 @@ const styles = StyleSheet.create({
     position: 'relative',
     display: 'flex',
     flex: 1,
-  }, image: {
+  },
+  image: {
     flex: 1,
     resizeMode: 'cover',
     height: '100%',
-  }
+  },
 });
 
 export default FriendEmergencyContacts;

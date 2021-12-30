9b677bb406e9fb11a4c158a30162498f9770b73b
iosexperts <iosexperts@rvtechnologies.com>
2021-12-30 16:30:57 +0530

    servey list

--- src/screens/Drawerscreens/Survey/QustionAnswer.js
+++ src/screens/Drawerscreens/Survey/QustionAnswer.js
@@ -1,106 +1,75 @@
-import React, {useState , useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
-  Image,
   ImageBackground,
   SafeAreaView,
   Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
-import SegmentedControl from 'rn-segmented-control';
 import {moderateScale} from 'react-native-size-matters';
+import {useDispatch, useSelector} from 'react-redux';
+import SegmentedControl from 'rn-segmented-control';
 import {fonts, icons} from '../../../../assets';
-import {Button} from '../../../components/common/Button';
+import {Loader} from '../../../components/common';
 import {Header} from '../../../components/common/Header';
 import {strings} from '../../../localization';
+import {questionsurvey} from '../../../store/actions';
 import {colors} from '../../../utilities/constants';
 import {layout} from '../../../utilities/layout';
 import styles from './styles';
-import {useDispatch, useSelector} from 'react-redux';
-import { questionsurvey } from '../../../store/actions';
-
-let members = [
-  {
-    img: icons.signin_bg_ic,
-    name: 'DO YOU CURRENTLU HOLD A COMMERCIAL MARINE LICENSE (CML) ?',
-    date: 'September 27,2021 2:21 PM',
-    text: 'Export to Photosharing',
-    srno: 'Q 1:',
-  },
-  {
-    img: icons.signin_bg_ic,
-    srno: 'Q 2:',
-    name: 'HAVE YOU READ THE PROPOSED AMENDMENTS IN BOTH BILLS ?',
-    date: 'September 27,2021 2:21 PM',
-    text: 'Export to Photosharing',
-  },
-  {
-    img: icons.signin_bg_ic,
-    srno: 'Q 3:',
-    name: 'DO YOU SUPPOT THE INCLUSION OF REQUIRING CMLs FOR "GUIDE" SERVICES ?',
-    date: 'September 27,2021 2:21 PM',
-    text: 'Export to Photosharing',
-  },
-  {
-    img: icons.signin_bg_ic,
-    srno: 'Q 4:',
-    name: 'DO YOU SUPPORT ESTABLISHING COMMERCIAL MARINE VESSEL LICENSES (CMVLs) ?',
-    date: 'September 27,2021 2:21 PM',
-    text: 'Export to Photosharing',
-  },
-];
 
 const QuestionAnswer = ({navigation}) => {
-  const [membersList, setMembersList] = useState([]);
-  const [tabIndex, setTabIndex] = React.useState(1);
-  const [theme, setTheme] = React.useState('LIGHT');
+  const [questionList, setquestionList] = useState([]);
+  const [tabIndex, setTabIndex] = useState(1);
 
   let auth = useSelector(state => state.auth);
   let app = useSelector(state => state.app);
-  console.log(auth, 'auth>>>>>>>>>>>>', app, 'app>>>>>>>>>>>>>>>>');
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('in useEfectof news>>>>>>>>>>>.');
+    console.log('in useEfectof ques>>>>>>>>>>>.');
     const unsubscribe = navigation.addListener('focus', () => {
       surevyfunc();
     });
     return unsubscribe;
   }, [navigation]);
 
-  //get news taken list
   function surevyfunc() {
     let ob = {};
     ob.token = auth && auth?.userDetails?.access_token;
-    dispatch
-    // (questionsurvey(token));
-   ( questionsurvey(ob, cb => {
-      if (cb) {
-        console.log(cb, 'in Questionpage  page>>>>>>>>>');
-        if (cb?.data?.data) {
-          let fishArr = cb?.data?.data?.questions;
+    dispatch(
+      questionsurvey(ob, cb => {
+        if (cb) {
+          console.log(cb, 'in Questionpage  page>>>>>>>>>');
+          if (cb?.data?.data) {
+            let questionArr = cb?.data?.data?.questions;
 
-          // fishArr.forEach(element => {
-          //   element.imgUrl = cb && cb.data && cb.data.base_url;
-          // });
-          setMembersList(fishArr);
-          // setTabIndex(fishArr)
+            questionArr.forEach(element => {
+              element.isAns = 0;
+            });
+            console.log(questionArr, 'questionArrquestionArrquestionArr');
+            setquestionList(questionArr);
+          }
         }
-      }
-    }),
-   )
+      }),
+    );
   }
-  const toggleTheme = () =>
-    theme === 'LIGHT' ? setTheme('DARK') : setTheme('LIGHT');
-  const handleTabsChange = (index) => {
-    console.log(index , 'mmmmmmmmmmmmmmmmmm');
-    setTabIndex(index);
+
+  const handleTabsChange = (v, i) => {
+    const tempArr = questionList.slice();
+
+    if (tempArr[i].isAns == 1) {
+      tempArr[i].isAns = 0;
+    } else {
+      tempArr[i].isAns = 1;
+    }
+    console.log(tempArr, 'tempArr arr');
+    setquestionList(tempArr);
   };
 
   const _renderView = ({item, index}) => (
     <View style={{flex: 1}}>
-      <TouchableOpacity
+      <View
         style={[
           styles.listView,
           {
@@ -108,17 +77,6 @@ const QuestionAnswer = ({navigation}) => {
           },
         ]}
         activeOpacity={0.8}>
-        {/* <View
-              style={{
-                
-                flexDirection: 'row',
-                flex: 0.9,
-                backgroundColor:colors.black1
-              }}>
-              <Button
-                style={{}}
-              />
-            </View> */}
         <View style={styles.viewStyle}>
           <View style={{alignSelf: 'center'}}>
             <View style={{flexDirection: 'row', bottom: moderateScale(10)}}>
@@ -127,34 +85,29 @@ const QuestionAnswer = ({navigation}) => {
             </View>
           </View>
         </View>
-        <View style={{flexDirection:'row'}}>
+        <View style={{flexDirection: 'row'}}>
           <SegmentedControl
-          tabs={['Yes', 'No']}
-          onChange={() => {}}
-          paddingVertical={6}
-          segmentedControlBackgroundColor="#86c4fD"
-          activeSegmentBackgroundColor="#0482f7"
-          activeTextColor="white"
-          textColor="black"
-          width={layout.size.width - 50}
-          containerStyle={{
-            marginVertical: 5,
-            top: 1,
-            color: colors.primary,
-          }}
-          textStyle={{
-            fontFamily: fonts.bold,
-            fontSize: 20,
-            color: colors.white1,
-          }}
-          currentIndex={tabIndex}
-          onChange={handleTabsChange}
-          theme={theme}
-        />
-         
-        
+            tabs={['Yes', 'No']}
+            paddingVertical={6}
+            segmentedControlBackgroundColor="#86c4fD"
+            activeSegmentBackgroundColor="#0482f7"
+            activeTextColor="white"
+            textColor="black"
+            width={layout.size.width - 50}
+            containerStyle={{
+              marginVertical: 5,
+              top: 1,
+            }}
+            textStyle={{
+              fontFamily: fonts.bold,
+              fontSize: 20,
+              color: colors.white1,
+            }}
+            currentIndex={item.isAns}
+            onChange={() => handleTabsChange(item, index)}
+          />
         </View>
-      </TouchableOpacity>
+      </View>
       <View
         style={{
           height: 2,
@@ -189,17 +142,19 @@ const QuestionAnswer = ({navigation}) => {
         />
 
         <FlatList
-          extraData={membersList}
-          data={membersList}
+          extraData={questionList}
+          data={questionList}
           renderItem={_renderView}
           keyExtractor={(item, index) => 'key' + index}
           ListHeaderComponent={() =>
-            !membersList.length ? (
+            !questionList.length ? (
               <Text style={styles.nomatch}>No Match found</Text>
             ) : null
           }
         />
       </SafeAreaView>
+
+      <Loader isAbsolute isLoading={app.loading} />
     </ImageBackground>
   );
 };

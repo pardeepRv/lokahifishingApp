d36f7021ce2ac56aa60359144d6bd394d40cff82
user <user@kunal-system.local>
2022-01-03 18:19:53 +0530

    Added info.plist

--- ios/lokahifishinglatest/AppDelegate.m
+++ ios/lokahifishinglatest/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -50,6 +50,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
